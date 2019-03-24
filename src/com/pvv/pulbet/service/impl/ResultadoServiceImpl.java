d83c199580bdedee63bc7fd591030fcd9b752569
Pablo <Pablo@192.168.0.15>
2019-03-24 21:31:47 +0100

    ResultadoService

--- src/com/pvv/pulbet/service/impl/ResultadoServiceImpl.java
+++ src/com/pvv/pulbet/service/impl/ResultadoServiceImpl.java
@@ -1,13 +1,29 @@
 package com.pvv.pulbet.service.impl;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.pvv.pulbet.dao.ResultadoDAO;
+import com.pvv.pulbet.dao.impl.ResultadoDAOImpl;
+import com.pvv.pulbet.dao.util.ConnectionManager;
+import com.pvv.pulbet.dao.util.JDBCUtils;
 import com.pvv.pulbet.exceptions.DataException;
 import com.pvv.pulbet.exceptions.InstanceNotFoundException;
 import com.pvv.pulbet.model.Resultado;
 import com.pvv.pulbet.service.ResultadoService;
 
 public class ResultadoServiceImpl implements ResultadoService{
+	
+	private ResultadoDAO resultadoDAO = null;
+	private static Logger logger = LogManager.getLogger(ResultadoServiceImpl.class);
+	
+	public ResultadoServiceImpl() {
+		resultadoDAO = new ResultadoDAOImpl();
+	}
 
 //	@Override
 //	public Resultado create(Resultado r) throws DuplicateInstanceException, DataException {
@@ -23,8 +39,26 @@ public class ResultadoServiceImpl implements ResultadoService{
 
 	@Override
 	public Resultado findById(Integer id, String idioma) throws InstanceNotFoundException, DataException {
-		// TODO Auto-generated method stub
-		return null;
+
+		if(logger.isDebugEnabled()) {
+			logger.debug("Id = {}", id);
+		}
+		
+		Connection connection = null;
+
+		try {
+
+			connection = ConnectionManager.getConnection();
+			connection.setAutoCommit(true);
+
+			return resultadoDAO.findById(connection, id, idioma);
+
+		} catch (SQLException e){
+			logger.warn(e.getMessage(), e);
+			throw new DataException(e);
+		} finally {
+			JDBCUtils.closeConnection(connection);
+		}
 	}
 
 	@Override
